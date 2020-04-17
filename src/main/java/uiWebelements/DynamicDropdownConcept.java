5b5da69f6462e66974f18bce8c10b828cee7b0d3
Amol Pardhi <amolpardhi9@gmail.com>
2020-04-17 18:23:44 +0530

    code added

--- src/main/java/uiWebelements/DynamicDropdownConcept.java
+++ src/main/java/uiWebelements/DynamicDropdownConcept.java
@@ -2,6 +2,7 @@ package uiWebelements;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,17 +10,27 @@ public class DynamicDropdownConcept {
 	
 	static WebDriver driver;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		System.setProperty("webdriver.chrome.driver", ".\\browsers\\chromedriver.exe");
 		System.setProperty("webdriver.chrome.silentOutput", "true");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().pageLoadTimeout(25, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.get("https://www.spicejet.com/");
 		
+		driver.findElement(By.xpath("//input[@id='ctl00_mainContent_ddl_originStation1_CTXT']")).click();
+		driver.findElement(By.xpath("//a[contains(text(),' Bhopal (BHO)')]")).click();
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[contains(text(),' Pune (PNQ)')]")).click();
+		
+		
+		Thread.sleep(2000);
+		driver.quit();
 	}
-
 }
+
+//Parent Child Relationship xpath -- //div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[contains(text(),' Pune (PNQ)')]
