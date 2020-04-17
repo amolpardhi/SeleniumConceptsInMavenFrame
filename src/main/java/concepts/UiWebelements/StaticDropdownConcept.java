00437540199722c88fdf9e0dd34c7ff8c824af94
Amol Pardhi <amolpardhi9@gmail.com>
2020-04-17 18:56:42 +0530

    First code commit

---
+++ src/main/java/concepts/UiWebelements/StaticDropdownConcept.java
@@ -0,0 +1,27 @@
+package concepts.UiWebelements;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+//static dropdown -- 
+
+public class StaticDropdownConcept {
+
+	static WebDriver driver;
+	
+	public static void main(String[] args) {
+	
+		System.setProperty("webdriver.chrome.driver", ".\\browsers\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.silentOutput", "true");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("https://www.spicejet.com/");
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+	}
+	
+}
